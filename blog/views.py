f11424700b1d64ce4df1282e713706dd54698055
Micaela <melomicaelaa@gmail.com>
2016-12-08 00:41:56 -0300

    mofides

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,8 @@ from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from .forms import PostForm
+from django.shortcuts import redirect
+
 
 # Create your views here.
 
