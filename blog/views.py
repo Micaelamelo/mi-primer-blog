ca249d337d92d3ce24bb051d0ef90720acef3657
Micaela <melomicaelaa@gmail.com>
2016-12-08 01:01:00 -0300

    mofideasdass

--- blog/views.py
+++ blog/views.py
@@ -28,6 +28,11 @@ def post_new(request):
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 	
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
+	
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
